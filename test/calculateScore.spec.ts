c2324722ef0202e5404e1907b1b2dee124d6c296
jannj <jannjacobson1997@gmail.com>
2022-04-11 16:31:41 +0200

    add vitest and 2 basic unit tests

---
+++ test/calculateScore.spec.ts
@@ -0,0 +1,76 @@
+import { describe, expect, it } from 'vitest';
+
+import calculateScore from '../src/calculcateScore';
+
+describe('CalculateScore method', () => {
+  it('calculates the score', () => {
+    const network1 = {
+      nodes: ['a'],
+      edges: [
+        {
+          source: 'a',
+          target: 'a',
+          weight: 2,
+        },
+      ],
+    };
+
+    expect(calculateScore(network1)).toBe(2);
+
+    const network2 = {
+      nodes: ['a', 'b'],
+      edges: [
+        {
+          source: 'a',
+          target: 'a',
+          weight: 1,
+        },
+        {
+          source: 'a',
+          target: 'b',
+          weight: 2,
+        },
+        {
+          source: 'b',
+          target: 'a',
+          weight: 1,
+        },
+        {
+          source: 'b',
+          target: 'b',
+          weight: 1,
+        },
+      ],
+    };
+
+    expect(calculateScore(network2)).toBe(3);
+
+    const network3 = {
+      nodes: ['a', 'b', 'c'],
+      edges: [
+        {
+          source: 'a',
+          target: 'c',
+          weight: 42,
+        },
+        {
+          source: 'c',
+          target: 'a',
+          weight: 1000,
+        },
+        {
+          source: 'c',
+          target: 'b',
+          weight: 300,
+        },
+        {
+          source: 'b',
+          target: 'a',
+          weight: 37,
+        },
+      ],
+    };
+
+    expect(calculateScore(network3)).toBe(1337);
+  });
+});
---
+++ test/generateProgressNetwork.spec.ts
@@ -0,0 +1,109 @@
+import { describe, expect, it } from 'vitest';
+
+import { Trace } from '../src';
+import generateProgressNetwork from '../src/generateProgressNetwork';
+
+describe('GenerateProgressNetwork method', () => {
+  it('generates network', () => {
+    const tests = <string[]>[
+      'T1',
+      'T2',
+    ];
+    const traces = <Trace[]>[
+      // student 1
+      {
+        attempts: [
+          {
+            hasPassed: false,
+            failedTestIndex: 0,
+          },
+        ],
+      },
+      // student 2
+      {
+        attempts: [
+          {
+            hasPassed: false,
+            failedTestIndex: 1,
+          },
+          {
+            hasPassed: false,
+            failedTestIndex: 1,
+          },
+          {
+            hasPassed: true,
+          },
+        ],
+      },
+      // student 3
+      {
+        attempts: [
+          {
+            hasPassed: false,
+            failedTestIndex: 0,
+          },
+          {
+            hasPassed: true,
+          },
+        ],
+      },
+    ];
+
+    const expectedNetwork = {
+      nodes: [
+        'start',
+        'T1',
+        'T2',
+        'PASS',
+        'end',
+      ],
+      edges: [
+        {
+          source: 'start',
+          target: 'T1',
+          weight: 2,
+        },
+        {
+          source: 'start',
+          target: 'T2',
+          weight: 1,
+        },
+        {
+          source: 'T1',
+          target: 'PASS',
+          weight: 1,
+        },
+        {
+          source: 'T1',
+          target: 'end',
+          weight: 1,
+        },
+        {
+          source: 'T2',
+          target: 'T2',
+          weight: 1,
+        },
+        {
+          source: 'T2',
+          target: 'PASS',
+          weight: 1,
+        },
+        {
+          source: 'PASS',
+          target: 'end',
+          weight: 2,
+        },
+      ],
+    };
+
+    const generatedNetwork = generateProgressNetwork(tests, traces);
+
+    expect(generatedNetwork.nodes).toStrictEqual(expectedNetwork.nodes);
+
+    expect(generatedNetwork.edges.length).toBe(expectedNetwork.edges.length);
+
+    expectedNetwork.edges.forEach((edge) => {
+      expect(generatedNetwork.edges).toContainEqual(edge);
+    });
+  });
+});
