4eccc7b4646a5cb179fc5f5f315a18b40db8478b
jannj <jannjacobson1997@gmail.com>
2022-04-21 23:12:16 +0200

    bundle only necessary d3 components

---
+++ src/d3-bundle.ts
@@ -0,0 +1,3 @@
+export { select } from 'd3-selection';
+export { scaleBand } from 'd3-scale';
+export { line, curveNatural } from 'd3-shape';
--- src/renderProgressNetwork.ts
+++ src/renderProgressNetwork.ts
@@ -1,5 +1,4 @@
-import * as d3 from 'd3';
-
+import * as d3 from './d3-bundle';
 import { Edge, ProgressNetwork } from './types';
 
 const renderProgressNetwork = (
